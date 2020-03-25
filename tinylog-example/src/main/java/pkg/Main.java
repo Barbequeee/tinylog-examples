a3f3932257b32de2883c2dff15dfa6c40bbeed1f
Barbequeee <kismacsek198@gmail.com>
2020-03-25 17:16:21 +0100

    Paraméteres naplóüzenet, kivétel naplóüzenet

--- tinylog-example/src/main/java/pkg/Main.java
+++ tinylog-example/src/main/java/pkg/Main.java
@@ -10,5 +10,8 @@ public class Main {
         Logger.info("This is an INFO message.");
         Logger.debug("This is a DEBUG message.");
         Logger.trace("This is TRACE message.");
+
+        Logger.info("User's name is {}", System.getProperty("user.name"));
+        Logger.error(new RuntimeException("Oops"), "Something is wrong");
     }
 }
